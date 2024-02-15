3012f029f42330ebb859df91e23f55f11e842b05
StudentUserDiego03 <dcardenascastane@student.alamo.edu>
2024-02-15 04:06:52 -0600

    created functions

---
+++ app.js
@@ -0,0 +1,24 @@
+
+function colors(){
+let inputText = document.getElementById("color_choice").value;
+if (inputText=="red"){
+    document.getElementById("color").style.color = "red";
+}
+if (inputText=="blue"){
+    document.getElementById("color").style.color = "blue";
+}
+if (inputText=="green"){
+    document.getElementById("color").style.color = "green";
+}
+}
+
+function ranks(){
+    let text = "";
+    let colorNames = ["red", "blue", "green", "yellow"];
+    for (i=0; i<colorNames.length; i++ ){
+        text += colorNames[i] + "<br>";
+    }
+    document.getElementById("ranks").innerHTML=text;
+
+
+}
